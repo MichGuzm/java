3ee5046df87360dee1e01888764f8f68d45b113d
MichGuzm <michguzman2196@gmail.com>
2025-11-12 16:38:05 -0500

    rectangulo y cuadrado done

---
+++ Clases/src/com/krakedev/Cuadrado.java
@@ -0,0 +1,15 @@
+package com.krakedev;
+
+public class Cuadrado {
+	public int lado;
+	
+	public int calcularArea() {
+		int area=lado*lado;
+		return area;
+	}
+	
+	public int calcularPerimetro() {
+		int perimetro=lado*4;
+		return perimetro;
+	}
+}
